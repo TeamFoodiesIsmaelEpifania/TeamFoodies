485013f218321518bf3e61daccffe77010ee918a
ismaelnrqz <ismaelnrqz@gmail.com>
2024-01-23 13:33:15 -0500

    working fetch

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,13 @@
 import './style.css'
+import { region } from './utils/render'
+import { selectedRegion } from './utils/render'
 
 const main = () => {
   console.log('Testing')
   console.log('Testing branch')
   console.log('test #2')
+  region()
+  selectedRegion('Mexican')
 }
 
 main()
--- app/src/utils/render.js
+++ app/src/utils/render.js
@@ -1,1 +1,12 @@
 import { fetchData } from "./utils";
+
+export const region = async() => {
+  const eachRegion = await fetchData('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+  console.log(eachRegion)
+}
+
+export const selectedRegion = async(area) => {
+  const oneRegion = await fetchData(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
+  
+  console.log(oneRegion)
+}
