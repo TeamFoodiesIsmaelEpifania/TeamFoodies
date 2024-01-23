d3841dbf69d01e07933632500c958019c69e6f90
ismaelnrqz <ismaelnrqz@gmail.com>
2024-01-23 17:04:17 -0500

    base-work

--- app/src/main.js
+++ app/src/main.js
@@ -3,9 +3,6 @@ import { region } from './utils/render'
 import { selectedRegion } from './utils/render'
 
 const main = () => {
-  console.log('Testing')
-  console.log('Testing branch')
-  console.log('test #2')
   region()
   selectedRegion('Mexican')
 }
--- app/src/utils/render.js
+++ app/src/utils/render.js
@@ -1,9 +1,13 @@
 import { fetchData } from "./utils";
 
-export const region = async() => {
-  const eachRegion = await fetchData('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
-  console.log(eachRegion)
-}
+export const region = async () => {
+  const eachRegion = await fetchData('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+
+  const mainRegion = ['American', 'Spanish', 'Mexican', 'Chinese', 'Italian', 'Jamaican', 'British', 'French', 'Portuguese', 'Greek' ]
+  const filteredRegions = eachRegion.meals.filter((regions => mainRegion.includes(regions.strArea)))
+
+  console.log(filteredRegions)
+};
 
 export const selectedRegion = async(area) => {
   const oneRegion = await fetchData(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
