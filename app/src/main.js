68e58b977c0e80cf6222281f862b4443ca9d23d9
ismaelnrqz <ismaelnrqz@gmail.com>
2024-01-25 11:56:01 -0500

    lets do this again

--- app/src/main.js
+++ app/src/main.js
@@ -1,37 +1,19 @@
 import './style.css';
-import { apiFetch, region } from './utils/render';
+import { region, selectedRegion, randomMeals, clickForMore } from './utils/render';
 
-import { randomMeals } from './utils/render';
-import {
-  region,
-  selectedRegion,
-  randomMeals,
-  clickForMore,
-} from './utils/render';
 
 const main = (e) => {
   if (e) {
     e.preventDefault();
   }
 
-  region();
-
-  document
-    .querySelector('.dropdown-menu')
-    .addEventListener('click', (event) => {
-      const selectedRegionName = event.target.textContent.trim();
-      selectedRegion(selectedRegionName);
-    });
-  document
-    .querySelector('.dropdown-menu')
-    .addEventListener('click', (event) => {
-      const selectedRegionName = event.target.textContent.trim();
-      selectedRegion(selectedRegionName);
-    });
+  document.querySelector('.dropdown-menu').addEventListener('click', (event) => {
+    const selectedRegionName = event.target.textContent.trim();
+    selectedRegion(selectedRegionName);
+  });
 
   region();
   randomMeals(3);
-  // selectedRegion('American');
 };
 
 main();
