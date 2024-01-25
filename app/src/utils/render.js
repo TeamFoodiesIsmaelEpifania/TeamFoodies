e45c5f526f6669f72a2ee831d6b9b6d8a26d716a
ismaelnrqz <ismaelnrqz@gmail.com>
2024-01-24 19:06:11 -0500

    looking better

--- app/src/utils/render.js
+++ app/src/utils/render.js
@@ -6,18 +6,6 @@ export const region = async () => {
   const fiveRegions = eachRegion.meals.slice(0, 4)
 
   const listOfRegions = document.querySelector('.dropdown-menu')
-
-  const handleLiClick = async (selectedRegionCallback, regionName) => {
-    const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${regionName}`;
-    try {
-      const regionData = await fetchData(apiUrl);
-      console.log('Selected region Meals:', regionData.meals);
-      // Call the callback function with the region name
-      selectedRegionCallback(regionName);
-    } catch (error) {
-      console.error('Error fetching region data:', error);
-    }
-  };
   
   fiveRegions.forEach(region => {
     const li = document.createElement('li');
@@ -25,10 +13,6 @@ export const region = async () => {
     li.textContent = region.strArea;
 
     listOfRegions.append(li)
-
-    li.addEventListener('click', () => {
-      handleLiClick(selectedRegion, region.strArea);
-    });
   });
 };
 
@@ -39,13 +23,17 @@ export const selectedRegion = async (area) => {
   mealListContainer.innerHTML = '';
   oneRegion.meals.forEach((meal) => {
     const { idMeal, strMeal, strMealThumb } = meal;
-    const mealDiv = document.createElement('div');
+    const mealSec = document.createElement('section');
     const mealHTML = `
-      <img src="${strMealThumb}" alt="${strMeal}">
-      <p>Name: ${strMeal}</p>
-      <p>ID: ${idMeal}</p>`;
-    mealDiv.innerHTML = mealHTML;
-    mealListContainer.appendChild(mealDiv);
+    <div class='eachRender'>
+    <img src="${strMealThumb}" alt="${strMeal}" class="meal-image">
+    <p>Name: ${strMeal}</p>
+    <p>ID: ${idMeal}</p>
+    <button class='moreInfo'>Click for more info</button>
+    </div>
+    `;
+    mealSec.innerHTML = mealHTML;
+    mealListContainer.appendChild(mealSec);
   });
 };
 
